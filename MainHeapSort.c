35ceef05c99f91551727380c17debc45ca108ec8
RezaMaulana <117805571+RezaAziiz@users.noreply.github.com>
2023-05-07 17:41:16 +0700

    modifikasi agafr inputan dari keyboard

--- MainHeapSort.c
+++ MainHeapSort.c
@@ -5,18 +5,28 @@
 #include "BodySTACK.C"
 #include "BodyHeapSort.C"
 #include <stdio.h>
+#define MAXSIZE 100
 
 int main()
 {
 	BinTree myTree;
 	StackList sortedStack;
-	int tes[10] = {5, 2, 7, 4, 6, 1, 3, 9, 11, 10};
-	int i;
+//	int tes[10] = {5, 2, 7, 4, 6, 1, 3, 9, 11, 10};
+	int i,j,batas,tes[MAXSIZE];
 	
 	CreateTree (&myTree);
 	CreateStackList (&sortedStack);
 	
-	for (i=0;i<10;i++)
+	printf("Input size Queue: ");
+	scanf("%d", &batas);
+	
+	printf("Info Queue: ");
+	for(j=0;j<batas;j++){
+		scanf("%d", &tes[j]);
+		printf("Queue dengan info %d telah diinsert", tes[j]);
+	}
+	
+	for (i=0;i<batas;i++)
 	{
 		InsertNode (&myTree, tes[i]);
 	}
